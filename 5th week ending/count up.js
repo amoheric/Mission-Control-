da4c8ae4d58a143b1c1c831ef05abaad5315da68
amoheric <105559568+amoheric@users.noreply.github.com>
2022-06-11 14:11:29 -0400

    A Web Presence
    Now that we have spent all semester creating a web page, we want to create a web presence so parents, friends, family, and anyone else on the internet can view your handiwork.

---
+++ 5th week ending/count up.js
@@ -0,0 +1,13 @@
+    /*
+    ERIC AMOH ADJEI
+    END OF MID-TERM! Assigment.Js
+    6/11/22
+    */
+
+    for(let countdown = 0; countdown < 10; countdown++) // using the for loop function, which all variables are set with the for parenthesis
+    {
+        document.write("countdown in.." + countdown); //this is the return command of the loop countdown
+        document.write("<br></br>"); // just a line break to make reading code easy
+    }
+    
+        document.write("Mission Control, Blastoff Intercepted By Unknown Drones!!!"); // Executing what i need as the end messeage after my countdown is done
